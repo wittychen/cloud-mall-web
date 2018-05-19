b215b018239d8383f207ba6fdc0e4a6a51b29d36
chenfan <chenfan@guahao.com>
2018-05-19 19:08:15 +0800

    init

---
+++ src/main/java/com/wittychen/cloudmall/web/vo/UserVO.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2001-2018 GuaHao.com Corporation Limited. All rights reserved.
+ * This software is the confidential and proprietary information of GuaHao Company.
+ * ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only
+ * in accordance with the terms of the license agreement you entered into with GuaHao.com.
+ */
+package com.wittychen.cloudmall.web.vo;
+
+import lombok.Data;
+
+/**
+ * 用户VO
+ *
+ * @author chenfan
+ * @version V1.0
+ * @since 2018-05-15 15:38
+ */
+@Data
+public class UserVO {
+
+    private Long id;
+
+    private String name;
+
+}
